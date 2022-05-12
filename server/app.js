fa254f4263f6e2f303ceaabfd85f6f7167c37661
Igor Karpov <karpov.zgr@mail.ru>
2022-05-12 18:35:45 +0300

    singers get done

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
+const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const express = require('express');
 const morgan = require('morgan');
 const { sequelize } = require('./db/models');
 
 const singerRouter = require('./routes/singer.router');
-const songRouter = require('./routes/song.router');
+// const songRouter = require('./routes/song.router');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
@@ -17,8 +18,8 @@ app.use(cors({
   origin: ['*'],
 }));
 
-app.use('api/singers', singerRouter);
-app.use('api/songs', songRouter);
+app.use('/api/singers', singerRouter);
+// app.use('api/songs', songRouter);
 
 app.listen(PORT, async () => {
   console.log('Сервер запущен на порту:', PORT);
--- server/routes/singer.router.js
+++ server/routes/singer.router.js
@@ -1,11 +1,19 @@
 const Router = require('express');
-const Singer = require('../db/models');
+const { Singer } = require('../db/models');
 
 const router = new Router();
 
 router.get('/', async (req, res) => {
   try {
+    const singers = await Singer.findAll({
+      // where: {
+      //   id: 1,
+      // },
+      raw: true,
+      attributes: ['id', 'name'],
+    });
 
+    return res.status(200).json(singers);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
@@ -14,38 +22,45 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
+    const { id } = req.params;
+    const singer = await Singer.findOne({
+      where: { id },
+      raw: true,
+      attributes: ['id', 'name'],
+    });
 
+    return res.status(200).json(singer);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
+// router.post('/', async (req, res) => {
+//   try {
 
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+//   } catch (error) {
+//     console.log(error);
+//     res.sendStatus(500);
+//   }
+// });
 
-router.put('/:id', async (req, res) => {
-  try {
+// router.put('/:id', async (req, res) => {
+//   try {
 
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+//   } catch (error) {
+//     console.log(error);
+//     res.sendStatus(500);
+//   }
+// });
 
-router.delete('/:id', async (req, res) => {
-  try {
+// router.delete('/:id', async (req, res) => {
+//   try {
 
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+//   } catch (error) {
+//     console.log(error);
+//     res.sendStatus(500);
+//   }
+// });
 
 module.exports = router;
