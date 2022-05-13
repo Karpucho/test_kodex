c99379dc0ffa44b3f2ead3704397694dbc45cfcd
Igor Karpov <karpov.zgr@mail.ru>
2022-05-13 20:24:58 +0300

    bugs

--- client/src/components/AllSingers/AllSingers.jsx
+++ client/src/components/AllSingers/AllSingers.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
 import './allsingers.css'
+import { useLocation } from 'react-router-dom';
+
 
 function AllSingers(props) {
+
+  const { search: query } = useLocation()
+  const allSingersQuery = useQuery(`allSingers${query}`, () => axios({
+    url: `http://localhost:5000/api/singers${query}`
+  }), {
+    cacheTime:
+      query ? 0 : 2 * 60 * 1000
+  })
+
+  if (allSingersQuery.isLoading) {
+    return (
+      <div>Загрузка</div>
+    )}
+
+  if (allSingersQuery.isError) {
+    return (
+      <div>Ошибка</div>
+    )}
+  const singersList = allSingersQuery.data.data
   return (
     <div>
-      singers
+      {singersList.map(singer => <div key={singer.id}>{singer.name}</div>)}
     </div>
   );
 }
